62ee3fca09a0084ed4c64a7040f4c2a6d74d794f
JessyMin <uxinsight@naver.com>
2017-04-05 11:31:07 +0900

    Re-organizing
    names(subjects) <- "subject" line moved.

--- run_analysis.R
+++ run_analysis.R
@@ -22,26 +22,26 @@ trainActivities <- read.table("train/y_train.txt")
 testActivites <- read.table("test/y_test.txt")
 activities <- rbind(trainActivities, testActivites)
 
-trainSet <- read.table("train/X_train.txt")
-testSet <- read.table("test/X_test.txt")
-mergedSet <- rbind(trainSet, testSet)
+trainData <- read.table("train/X_train.txt")
+testData <- read.table("test/X_test.txt")
+mergedData <- rbind(trainSet, testSet)
 
 
 # 2.Load features and extract only the mean and standard deviation
 features <- read.table("features.txt")
 
 featuresSelected <- grep("mean\\(|std\\(", features[,2])
-mergedSet <- mergedSet[, featuresSelected]
-names(mergedSet) <- features[featuresSelected, 2]
+mergedData <- mergedData[, featuresSelected]
+names(mergedData) <- features[featuresSelected, 2]
 
-# 3. add descriptive activity names
+# 3. add descriptive activity names and labels
 activityLabel <- read.table("activity_labels.txt")
 activities[,1] <- activityLabel[activities[,1],2]
 names(activities) <- "activity"
-
-# 4. merge test/train datasets and add labels
 names(subjects) <- "subject"
-allData <- cbind(subjects, activities, mergedSet)
+
+# 4. merge test/train datasets
+allData <- cbind(subjects, activities, mergedData)
 write.table(allData, "allData.txt")
 
 # 5.creates a second, independent tidy data set 
